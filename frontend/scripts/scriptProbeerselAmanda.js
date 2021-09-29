3bfa2e8bd510543a8585608afc1247b533d28288
Amanda Schoonhoven <ahjl92@gmail.com>
2021-09-30 00:03:44 +0200

    Quick login screen to start the game & log-out

--- frontend/scripts/scriptProbeerselAmanda.js
+++ frontend/scripts/scriptProbeerselAmanda.js
@@ -1,6 +1,16 @@
 // Als je op volgende drukt veranderd de button in antwoorden inleveren
 function buttonNextClick(){
-    //console.log('Button clicked');
+    console.log('Button clicked');
     document.getElementById('next').textContent = 'Antwoorden inleveren';
 }
 
+function buttonLoginClick(){
+var name = document.querySelector('#name');
+    alert(name.value);
+    document.documentElement.textContent = 'index.html'
+}
+
+function buttonLogoutClick(){
+    document.documentElement.textContent = 'start.html';
+
+}
