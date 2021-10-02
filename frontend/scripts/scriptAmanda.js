fceab51f0b26657d5424bdada288aaa8e857c1d8
Amanda Schoonhoven <ahjl92@gmail.com>
2021-10-03 01:07:58 +0200

    Canvas round 1 is hardcoded ready to get functionality see ToDO.txt

--- frontend/scripts/scriptAmanda.js
+++ frontend/scripts/scriptAmanda.js
@@ -1,16 +1,35 @@
 // Als je op volgende drukt veranderd de button in antwoorden inleveren
-function buttonNextClick(){
+function openWhatToDoTab() {
     console.log('Button clicked');
     document.getElementById('next').textContent = 'Antwoorden inleveren';
 }
 
-function buttonLoginClick(){
+function buttonLoginClick() {
     const name = document.querySelector('#name');
     alert('Welkom ' + name.value);
     window.open('canvas.html', '_top')
 }
 
-function buttonLogoutClick(){
+function buttonLogoutClick() {
     alert('Tot ziens ');
     window.open('indexLogin.html', '_top')
+}
+
+function questionTab(evt, questionName) {
+    let i, questionContent, questionTabLink;
+    questionContent = document.getElementsByClassName("question-content");
+    for (i = 0; i < questionContent.length; i++) {
+        questionContent[i].style.display = "none";
+    }
+    questionTabLink = document.getElementsByClassName("question-tab-link");
+    for (i = 0; i < questionTabLink.length; i++) {
+        questionTabLink[i].className = questionTabLink[i].className.replace(" active", "");
+    }
+    document.getElementById(questionName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function biasPopup(biasName) {
+    const popup = document.getElementById(biasName);
+    popup.classList.toggle("show");
 }
