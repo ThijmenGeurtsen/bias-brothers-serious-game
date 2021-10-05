d12e3f64cd04b1a81c59966d8353e494cc0a83f6
Htou <hichem.touati@student.hu.nl>
2021-10-05 07:35:07 +0000

    Ternary Tree

---
+++ Backend/Node.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Node {
+    public int value;
+    public int subtree;
+
+    public Node left;
+    public Node middle;
+    public Node right;
+
+    public Node(int init) {
+        value = init;
+        subtree = 0;
+    }
+
+}
