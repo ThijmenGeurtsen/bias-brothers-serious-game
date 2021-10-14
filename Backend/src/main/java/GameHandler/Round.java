42a4bfbc728e4877fef421745f3448c1d6bd89b4
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-14 19:46:32 +0200

    Turned collections into arraylist

--- Backend/src/main/java/GameHandler/Round.java
+++ Backend/src/main/java/GameHandler/Round.java
@@ -1,23 +1,24 @@
 package main.java.GameHandler;
 
-import main.java.Canvas.BiasCollection;
-import main.java.Canvas.CanvasCollection;
-import main.java.Canvas.Scenario;
+import main.java.Canvas.*;
+
+import java.util.ArrayList;
 
 public class Round {
     private String roundNumber;
     private String roundTitle;
     private Timer timer;
     private Scenario scenario;
-    private BiasCollection biasCollection;
-    private MeasureQuestionCollection measureQuestionCollection;
-    private CanvasCollection canvasCollection;
+    private ArrayList<Bias> biasCollection = new ArrayList<Bias>();
+    private ArrayList<MeasureQuestion>  measureQuestionCollection;
+    private ArrayList<Canvas>  canvasCollection;
 
-    public Round(String roundNumber, String roundTitle, Timer timer, Scenario scenario, BiasCollection biasCollection, MeasureQuestionCollection measureQuestionCollection, CanvasCollection canvasCollection ) {
+    public Round(String roundNumber, String roundTitle, Timer timer, Scenario scenario, ArrayList<Bias> biasCollection, ArrayList<MeasureQuestion> measureQuestionCollection, ArrayList<Canvas> canvasCollection ) {
         this.roundNumber = roundNumber;
         this.roundTitle = roundTitle;
         this.timer = timer;
         this.scenario = scenario;
+        this.biasCollection = biasCollection;
         this.measureQuestionCollection = measureQuestionCollection;
         this.canvasCollection = canvasCollection;
     }
