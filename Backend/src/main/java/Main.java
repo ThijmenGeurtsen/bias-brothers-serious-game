11aec4669e6ebd514960fb97ff86e33c81fbd32a
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-14 18:23:41 +0200

    commented oud some functions to allow json exporting only

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -2,6 +2,7 @@ package main.java;
 
 import main.java.Utils.DataHandler;
 import main.java.Utils.Endpoints;
+import main.java.Utils.RoundCreator;
 
 import java.io.FileNotFoundException;
 
@@ -9,9 +10,11 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        //"http://localhost:4567/"
-        DataHandler dataHandler = new DataHandler();
-        Endpoints endpoints = new Endpoints(dataHandler);
-        endpoints.startServer();
+//        //"http://localhost:4567/"
+//        DataHandler dataHandler = new DataHandler();
+//        Endpoints endpoints = new Endpoints(dataHandler);
+//        endpoints.startServer();
+
+        RoundCreator.createRoundJSON();
     }
 }
