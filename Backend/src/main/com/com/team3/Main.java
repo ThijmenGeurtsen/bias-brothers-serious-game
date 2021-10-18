28cc27f894b2dd34ab5efa544855dc64b178245a
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-18 17:20:34 +0200

    Repaired File structure

---
+++ Backend/src/main/com/com/team3/Main.java
@@ -0,0 +1,19 @@
+package com.team3;
+
+import com.team3.utils.Endpoints;
+import com.team3.utils.DataHandler;
+
+import java.io.FileNotFoundException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    //"http://localhost:4567/"
+        DataHandler dataHandler = new DataHandler();
+        Endpoints endpoints = new Endpoints(dataHandler);
+        endpoints.startServer();
+    }
+
+}
