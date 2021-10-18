fed6258b89a13e6db65d7603961fc52eae89ad97
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-18 20:00:04 +0200

    space

--- Backend/src/main/com/com/team3/Main.java
+++ Backend/src/main/com/com/team3/Main.java
@@ -11,18 +11,22 @@ public class Main {
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
+
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        getHerokuAssignedPort();
+    public static void main(String[] args) {
+        try {
+            getHerokuAssignedPort();
 
-    //"http://localhost:4567/"
-        DataHandler dataHandler = new DataHandler();
-        Endpoints endpoints = new Endpoints(dataHandler);
-        endpoints.startServer();
+            //"http://localhost:4567/"
+            DataHandler dataHandler = new DataHandler();
+            Endpoints endpoints = new Endpoints(dataHandler);
+            endpoints.startServer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
