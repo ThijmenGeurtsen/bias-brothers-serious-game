da5fd8722988a47b6b113bda1e61e50aa325037c
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-20 12:52:09 +0200

    changed json files, but jar file is not finding them

--- Backend/src/main/java/com/team3/Main.java
+++ Backend/src/main/java/com/team3/Main.java
@@ -3,14 +3,13 @@ package com.team3;
 import com.team3.utils.Endpoints;
 import com.team3.utils.DataHandler;
 
-import java.io.FileNotFoundException;
-
 
 public class Main {
 
     public static void main(String[] args) {
+        
         try {
-
+            
             //"http://localhost:4567/"
             DataHandler dataHandler = new DataHandler();
             Endpoints endpoints = new Endpoints(dataHandler);
