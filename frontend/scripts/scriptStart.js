510ce24b3c77d20bbedb912ece17d3314db95528
ThijmenGeurtsen <thijmen01g@gmail.com>
2021-10-27 21:00:09 +0200

    removed timer line

--- frontend/scripts/scriptStart.js
+++ frontend/scripts/scriptStart.js
@@ -2,7 +2,8 @@ let input = sessionStorage.getItem('name');
 sessionStorage.setItem("round", 1);
 sessionStorage.setItem("endpointName", "round" + sessionStorage.getItem("round").toString());
 sessionStorage.setItem("canvasNumber", 2);
-sessionStorage.setItem("timerValue", 420000);
+
+
 document.getElementById("welcome-text").innerHTML = "Welkom " +
     input + ", bij de Serious Game!";
 
