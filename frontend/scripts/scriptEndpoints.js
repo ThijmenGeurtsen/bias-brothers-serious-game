dc29219c71aa8f9e22f4c655986af74e242dc9a2
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-28 17:40:15 +0200

    changed endpoints for testing

--- frontend/scripts/scriptEndpoints.js
+++ frontend/scripts/scriptEndpoints.js
@@ -1,7 +1,7 @@
 // Gets all the biases from the backend with an http request. Backend started with intelliJ.
 function fetchBiases() {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://seriousgame-env.eba-rqt9ruwy.eu-west-2.elasticbeanstalk.com/biases', true);
+    xhr.open('GET', 'http://localhost:5000/biases', true);
     xhr.onload = function () {
         if (this.status === 200) {
             const output = JSON.parse(this.responseText);
@@ -14,7 +14,7 @@ function fetchBiases() {
 function fetchRound() {
     let xhr = new XMLHttpRequest();
     let endpointName = sessionStorage.getItem("endpointName");
-    xhr.open('GET', 'http://seriousgame-env.eba-rqt9ruwy.eu-west-2.elasticbeanstalk.com/' + endpointName, true);
+    xhr.open('GET', 'http://localhost:5000/' + endpointName, true);
     xhr.onload = function () {
         if (this.status === 200) {
             const output = JSON.parse(this.responseText);
@@ -29,7 +29,7 @@ function fetchRound() {
 function fetchMeasure(round, canvasNumber, measureAnswer) {
     let xhr = new XMLHttpRequest();
     let endpointName = sessionStorage.getItem("endpointName");
-    xhr.open('GET', 'http://seriousgame-env.eba-rqt9ruwy.eu-west-2.elasticbeanstalk.com/' + "measure/" + round + "/" + canvasNumber + "/" + measureAnswer, true);
+    xhr.open('GET', 'http://localhost:5000/' + "measure/" + round + "/" + canvasNumber + "/" + measureAnswer, true);
     xhr.onload = function () {
         if (this.status === 200) {
             const output = JSON.parse(this.responseText);
