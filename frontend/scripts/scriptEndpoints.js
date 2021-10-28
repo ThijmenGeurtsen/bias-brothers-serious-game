72acf8b67129b2c89fda502647690eb9bf7f0ad1
Hichem Touati <hichem.touati@student.hu.nl>
2021-10-28 16:44:49 +0200

    Added fetchrequest for receiving canvasNumbers

--- frontend/scripts/scriptEndpoints.js
+++ frontend/scripts/scriptEndpoints.js
@@ -26,5 +26,20 @@ function fetchRound() {
     xhr.send();
 }
 
+function fetchMeasure(round, canvasNumber, measureAnswer) {
+    let xhr = new XMLHttpRequest();
+    let endpointName = sessionStorage.getItem("endpointName");
+    xhr.open('GET', 'http://seriousgame-env.eba-rqt9ruwy.eu-west-2.elasticbeanstalk.com/' + "measure/" + round + "/" + canvasNumber + "/" + measureAnswer, true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const output = JSON.parse(this.responseText);
+            loadGame(output)
+        } else {
+            console.log("Niet gevonden")
+        }
+    }
+    xhr.send();
+}
+
 // Start first canvas
 fetchRound();
