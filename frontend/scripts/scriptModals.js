8769e7f1f53bd289714fb86232c7c945a53bc912
Amanda Schoonhoven <ahjl92@gmail.com>
2021-10-31 15:01:33 +0100

    change src to html links

--- frontend/scripts/scriptModals.js
+++ frontend/scripts/scriptModals.js
@@ -11,7 +11,6 @@ function loadRoundWarningModals() {
         document.getElementById("informationBtn").style.display = "grid";
         let iframe = document.getElementById("iframe");
         checkCanvas(iframe);
-        iframe.att
         document.getElementById("information-table").appendChild(iframe);
     } else {
         document.getElementById("informationBtn").style.display = "none";
@@ -45,17 +44,19 @@ function checkCanvas(iframe){
     let canvasNumer = parseInt(sessionStorage.getItem("canvasNumber")) + 1;
     console.log(canvasNumer);
 
+    iframe.setAttribute(SameSite="Lax", "iframe");
+
     switch (canvasNumer) {
         case 1:
-            return iframe.src = "https://drive.google.com/file/d/1CWZcrZjt0kWoUQ0YPBc2eRF011kpXj-S/preview";
+            return iframe.src = "round4/Canvas 1.html";
         case 2:
-            return iframe.src = "https://drive.google.com/file/d/1xCR3xDrw8yJFzJNKttVE4fTIuKGRfAwq/preview";
+            return iframe.src = "round4/Canvas 2.html";
         case 3:
-            return iframe.src = "https://drive.google.com/file/d/1lMN0tctuVwQPsJGJgDOKozMVeVGXafL8/preview";
+            return iframe.src = "round4/Canvas 3.html";
         case 4:
-            return iframe.src = "https://drive.google.com/file/d/1IZknPUbU-mxo8i3gF-u_Rk1mC2_7Vhp4/preview";
+            return iframe.src = "round4/Canvas 4.html";
         case 5:
-            return iframe.src = "https://drive.google.com/file/d/115sW4vc252NxeVeL3moiKDJaXGAzYd40/preview";
+            return iframe.src = "round4/Canvas 5.html";
 
     }
 }
